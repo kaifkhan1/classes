61710eb7d2862ecf1ad3e9911c467a4c6cf4e5bc
kaifkhan1 <76866688+kaifkhan1@users.noreply.github.com>
2021-05-31 23:22:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function myOpen() {
+    let x = document.getElementById("mobile-nav");
+  //   var i = document.getElementsByClassName('bx bx-menu');
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
