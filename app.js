63b6dc89a872a42c0447d35c813b1eb294eb957f
kaifkhan1 <76866688+kaifkhan1@users.noreply.github.com>
2021-05-31 23:24:44 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function myOpen() {
-    let x = document.getElementById("mobile-nav");
-  //   var i = document.getElementsByClassName('bx bx-menu');
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
