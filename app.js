ca5f5f1bc66939b5d5e2e171d458361e809d6354
kaifkhan1 <76866688+kaifkhan1@users.noreply.github.com>
2021-06-02 23:16:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -68,11 +68,55 @@ function showSlides(n){
     slides[slideIndex-1].style.display = 'block';
 }
 
+// Nav Bar Animation 
 
 
+const navBar = document.querySelector('.nav-container');
+
+let scrolled = false;
+
+window.onscroll = function () {
+
+    if(window.pageYOffset > 100){
+        navBar.classList.remove('fixed');
+        if(!scrolled){
+            navBar.style.transform = 'translateY(-500px)'; 
+        }
+        setTimeout(function(){
+            navBar.style.transform = 'translateY(0)';
+            scrolled = true;
+        },200);
+
+    }
+    else{
+        navBar.classList.add('fixed');
+        scrolled = false;
+    }
+
+}
 
 
-
-
-
-// ScrolllReaveal 
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".nav-item").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
